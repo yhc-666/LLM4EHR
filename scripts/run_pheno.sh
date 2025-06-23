24b87d8598d6c713757b09c7de2e20f685d9293d
“yang” <hcy@mail.com>
2025-06-23 11:07:21 +0000

    completed lstm

--- scripts/run_pheno.sh
+++ scripts/run_pheno.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 # 在脚本中设置环境变量
-export DATA_ROOT="/home/ubuntu/hcy50662/output_mimic3"
+#export DATA_ROOT="/home/ubuntu/hcy50662/output_mimic3"
+export DATA_ROOT="/home/ubuntu/Virginia/output_mimic3"
 #python -m src.train --config config/pheno_clinical_longformer.yaml
-python -m src.train --config config/pheno_timellm.yaml
+#python -m src.train --config config/pheno_timellm.yaml
 #python -m src.train --config config/pheno_timellm_tsonly_freezebackbone.yaml
 #python -m src.train --config config/pheno_gpt4mts.yaml
 #python -m src.train --config config/pheno_clinical_bigbird.yaml
 #python -m src.train --config config/pheno_belt.yaml
+python -m src.train --config config/pheno_lstm.yaml
 
 
 
